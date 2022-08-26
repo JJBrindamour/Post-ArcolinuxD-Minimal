d0f96bf1df52295624996eebe6414e2321319718
JJ <jjbrindamour@gmail.com>
2022-08-26 00:42:41 -0400

    Delete test.sh

--- test.sh
+++
@@ -1,5 +0,0 @@
-for custom in ./.config/*; do
-    for preinstalled in "$HOME"/.config/*; do
-        if [[ $(basename $custom) == $(basename $preinstalled)  ]]; then echo $preinstalled; fi
-    done
-done
