e2ee494a955acef3104af8b19da06945bd80a5cb
JJ <jjbrindamour@gmail.com>
2022-08-26 13:00:12 -0400

    Fix package issue

--- install.sh
+++ install.sh
@@ -11,14 +11,14 @@ sudo pacman -S archlinux-keyring
 sudo pacman -Syyu
 
 # Install Pacman Packages
-sudo pacman -S pipewire pipewire-pulse pipewire-alsa pipewire-jack pamixer obconf ttf-font-awesome ttf-nerd-fonts-symbols-mono lxappearance kvantum grub-customizer openbox bspwm sxhkd sddm alacritty vim nano micro thunar geany sddm zsh picom xfce4-power-manager xfce4-settings dunst wget neofetch nitrogen plank polybar ranger rofi starship firefox arandr carla flatpak wine betterlockscreen spotify cava networkmanager-dmenu-git papirus-folders-nordic
+sudo pacman -S pipewire pipewire-pulse pipewire-alsa pipewire-jack pamixer obconf ttf-font-awesome ttf-nerd-fonts-symbols-mono lxappearance kvantum grub-customizer openbox bspwm sxhkd sddm alacritty vim nano micro thunar geany sddm zsh picom xfce4-power-manager xfce4-settings dunst wget neofetch nitrogen plank polybar ranger rofi starship firefox arandr carla flatpak wine betterlockscreen spotify cava papirus-folders-nordic
 
 # Install Yay
 #git clone https://aur.archlinux.org/yay.git "$HOME"/yay
 #(cd "$HOME"/yay && makepkg -si)
 
 # Install AUR Packages
-#yay -S betterlockscreen spotify cava networkmanager-dmenu-git papirus-folders-nordic # IN ARCO REPOS
+yay -S networkmanager-dmenu-git # betterlockscreen spotify cava papirus-folders-nordic # IN ARCO REPOS
 
 # Install Configs
 mv "$HOME"/.config "$HOME"/.config-backup
